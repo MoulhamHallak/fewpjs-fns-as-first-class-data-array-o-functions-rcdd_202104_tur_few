e91ccd91b76647285e450f565fa12a913b0a1614
Moulham Hallak <mou990@gmail.com>
2021-04-23 22:04:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+let routine = ["wakeDog", "leashDog", "walkToPark", "throwFrisbee", "walkHome", "unleashDog"];
+
+function routineDogs(){
+  for (let i=0; i<routine.length; i++){
+    function(i)
+  }
+}
+
+ 
+function wakeDog(routine[0]) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function exerciseDog(dogName, dogBreed){
+
+}
