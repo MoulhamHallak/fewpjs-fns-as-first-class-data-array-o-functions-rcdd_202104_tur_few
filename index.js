0f09d295087ac1363bbb02e6f9dd7a50ec408de6
Moulham Hallak <mou990@gmail.com>
2021-04-23 22:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-
-function wakeDog(dogName, dogBreed) {
-  const dogName = "";
-  const dogBreed = ""
+function wakeDog() {
+  const dogName = "Xerox";
+  const dogBreed = "Border Collie";
   return(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function leashDog() {
+  const dogName = "Boo Radley";
+  const dogBreed = "Pibble";
+  return(`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark() {
+  const dogName = "Bunny";
+  const dogBreed = "Labrador";
+  return(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee() {
+  const dogName = "Mary J. Blige";
+  const dogBreed = "Papillon";
+  return(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+function walkHome() {
+  const dogName = "Santa Paws";
+  const dogBreed = "Husky";
+  return(`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog() {
+  const dogName = "Lauren";
+  const dogBreed = "Golden Retriever";
+  return(`Unleash ${dogName} the ${dogBreed}`);
 }
 
